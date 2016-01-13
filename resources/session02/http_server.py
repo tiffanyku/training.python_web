891ca5cb0ca34cfff1b1e291fa60d1c4348688f7
Tiffany Ku <tiffany.ku3@gmail.com>
2016-01-12 21:05:44 -0800

    updates to http_server.py from class

--- resources/session02/http_server.py
+++ resources/session02/http_server.py
@@ -1,8 +1,33 @@
 import socket
 import sys
 
+def response_ok():
+    #returns a basic HTTP response
+    resp = []
+    resp.append(b"HTTP/1.1 200 OK")
+    resp.append(b"Content-Type: text/plain")
+    resp.append(b"")
+    resp.append(b"this is a pretty minimal response")
+    return b"\r\n".join(resp)
+
+def response_method_not_allowed():
+    #returns a 405 Method Not Allowed Response
+    resp = []
+    resp.append(b"HTTP/1.1 405 Method Not Allowed")
+    resp.append(b"")
+    return b"\r\n".join(resp)
+
+def parse_request(request):
+    #raise error if request method is not GET
+    first_line = request.split("\r\n", 1)[0]
+    method, uri, protocol = first_line.split()
+    if method != "GET":
+        raise NotImplementedError("We only accept GET")
+    print (sys.stderr, 'request is ok')
+    return uri
 
 def server(log_buffer=sys.stderr):
+    #create a server
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -16,16 +41,24 @@ def server(log_buffer=sys.stderr):
             conn, addr = sock.accept()  # blocking
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                request = ""
                 while True:
-                    data = conn.recv(16)
-                    print('received "{0}"'.format(data), file=log_buffer)
-                    if data:
-                        print('sending data back to client', file=log_buffer)
-                        conn.sendall(data)
-                    else:
-                        msg = 'no more data from {0}:{1}'.format(*addr)
-                        print(msg, log_buffer)
+                    data = conn.recv(1024)
+                    request += data.decode('utf8')
+                    if len(data) < 1024 or not data:
                         break
+                try:
+                    uri = parse_request(request)
+                except NotImplementedError:
+                    response = response_method_not_allowed()
+                else:
+                    try:
+                        content, mime_type = resolve_uri(url)
+                    except NameError:
+                        response = response_not_found()
+                    else:
+                        response = response_ok(content, mime_type)
+
             finally:
                 conn.close()
 
